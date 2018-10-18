958c62e0d68f4b9ff13f5436d51a4240df0efd00
brent-swaenen <brent.swaenen@student.ap.be>
2018-10-18 11:09:40 +0200

    puzzelverzameling

---
+++ Src/API/API/Model/PuzzelVerzameling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class PuzzelVerzameling
+    {
+        int ID;
+        string naam;
+        List<Puzzel> puzzels;
+
+        public PuzzelVerzameling(string naam, List<Puzzel> puzzels)
+        {
+            this.naam = naam;
+            this.puzzels = puzzels;
+        }
+    }
+}
