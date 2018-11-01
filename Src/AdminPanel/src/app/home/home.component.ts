d95a34df852ec791f79aef925c41a91c4d347eb3
Yorick Lardenoit <yorick.lardenoit@hotmail.com>
2018-11-01 20:20:59 +0100

    can add markers on map

--- Src/AdminPanel/src/app/home/home.component.ts
+++ Src/AdminPanel/src/app/home/home.component.ts
@@ -14,5 +14,19 @@ export class HomeComponent {
   title: string = 'Click to add places';
   lat: number = 51.228418;
   lng: number = 4.4159007;
+
+  markers: position[] = new Array
+  clickMap($event){
+    console.log($event.coords.lat);
+    console.log($event.coords.lng);
+    this.markers.push({
+      lat:$event.coords.lat,
+      lng:$event.coords.lng
+    });
+  }
   
+}
+export interface position{
+  lat: number,
+  lng: number
 }
