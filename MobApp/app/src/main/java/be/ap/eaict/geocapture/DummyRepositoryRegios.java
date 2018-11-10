481b9331b5da25e91a46bb4248d254d44a9fabf4
brent-swaenen <brent.swaenen@student.ap.be>
2018-11-10 15:41:16 +0100

    database regio toegevoegd

--- MobApp/app/src/main/java/be/ap/eaict/geocapture/DummyRepositoryRegios.java
+++ MobApp/app/src/main/java/be/ap/eaict/geocapture/DummyRepositoryRegios.java
@@ -18,32 +18,56 @@ public class DummyRepositoryRegios implements IDummyRepositoryRegios{
 
     @Override
     public List<Regio> getRegios() {
-        List<Regio> regios = new ArrayList<>();
-        regios.add(
-            new Regio(
-            "Antwerpen",
-                new ArrayList<Locatie>(){{
-                    add(new Locatie(
-                            "kerk",
-                            new ArrayList<Puzzel>(){{
-                                add(new Puzzel("wat wordt het meetst verkocht in starbucks", "drankje 3"));
-                                add(new Puzzel("hoe ver kan je geraken met een enkele trein(locatie)", "destinatie x"));
-                                add(new Puzzel("wa uur is locket x open", "5:50"));
-                            }},
-                            51.217263f,4.421034f
-                    ));
-                    add(new Locatie(
-                            "kerk",
-                            new ArrayList<Puzzel>(){{
-                                add(new Puzzel("wat is beeld x", "5"));
-                                add(new Puzzel("wanneer is beeld x", "2"));
-                                add(new Puzzel("waarom is dit een ding", "nee"));
-                            }},
-                            51.217263f,4.421034f
-                    ));
-                }},
-                60*60*5
-                ));
+        List<Regio> regios = new ArrayList<Regio>(){{
+            add(new Regio(
+                    "Antwerpen",
+                    new ArrayList<Locatie>(){{
+                        add(new Locatie(
+                                "kerk",
+                                new ArrayList<Puzzel>(){{
+                                    add(new Puzzel("wat wordt het meetst verkocht in starbucks", "drankje 3"));
+                                    add(new Puzzel("hoe ver kan je geraken met een enkele trein(locatie)", "destinatie x"));
+                                    add(new Puzzel("wa uur is locket x open", "5:50"));
+                                }},
+                                51.217263f,4.421034f
+                        ));
+                        add(new Locatie(
+                                "kerk",
+                                new ArrayList<Puzzel>(){{
+                                    add(new Puzzel("wat is beeld x", "5"));
+                                    add(new Puzzel("wanneer is beeld x", "2"));
+                                    add(new Puzzel("waarom is dit een ding", "nee"));
+                                }},
+                                51.217263f,4.421034f
+                        ));
+                    }},
+                    60*60*5
+            ));
+            add(new Regio(
+                    "Brasschaat",
+                    new ArrayList<Locatie>(){{
+                        add(new Locatie(
+                                "kerk",
+                                new ArrayList<Puzzel>(){{
+                                    add(new Puzzel("wie is hier het meest bekend", "yorick"));
+                                    add(new Puzzel("wat doet deze persoon", "lijntje coca cola"));
+                                    add(new Puzzel("waarom", "steen"));
+                                }},
+                                51.217263f,4.421034f
+                        ));
+                        add(new Locatie(
+                                "kerk",
+                                new ArrayList<Puzzel>(){{
+                                    add(new Puzzel("wat is beeld y", "19"));
+                                    add(new Puzzel("wanneer is beeld t", "2"));
+                                    add(new Puzzel("waarom is dit een iets", "misschien"));
+                                }},
+                                51.217263f,4.421034f
+                        ));
+                    }},
+                    60*60*5
+            ));
+        }};
 
         return regios;
     }
