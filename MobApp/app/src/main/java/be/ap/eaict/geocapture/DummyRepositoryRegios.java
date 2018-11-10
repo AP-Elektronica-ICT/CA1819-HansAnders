ffa67747a8505eb5da1cf7d964288e2b574251fb
brent-swaenen <brent.swaenen@student.ap.be>
2018-11-10 17:12:46 +0100

    locaties lat lng updates

--- MobApp/app/src/main/java/be/ap/eaict/geocapture/DummyRepositoryRegios.java
+++ MobApp/app/src/main/java/be/ap/eaict/geocapture/DummyRepositoryRegios.java
@@ -26,7 +26,7 @@ public class DummyRepositoryRegios implements IDummyRepositoryRegios{
                     "Antwerpen",
                     new ArrayList<Locatie>(){{
                         add(new Locatie(
-                                "kathedraal",
+                                "station",
                                 new ArrayList<Puzzel>(){{
                                     add(new Puzzel("wat wordt het meetst verkocht in starbucks", "drankje 3"));
                                     add(new Puzzel("hoe ver kan je geraken met een enkele trein(locatie)", "destinatie x"));
@@ -35,13 +35,22 @@ public class DummyRepositoryRegios implements IDummyRepositoryRegios{
                                 51.217263f,4.421034f
                         ));
                         add(new Locatie(
-                                "kerk",
+                                "kathedraal",
                                 new ArrayList<Puzzel>(){{
                                     add(new Puzzel("wat is beeld x", "5"));
                                     add(new Puzzel("wanneer is beeld x", "2"));
                                     add(new Puzzel("waarom is dit een ding", "nee"));
                                 }},
-                                51.217263f,4.421034f
+                                51.220236f, 4.402139f
+                        ));
+                        add(new Locatie(
+                                "KOT",
+                                new ArrayList<Puzzel>(){{
+                                    add(new Puzzel("wat is beeld x", "5"));
+                                    add(new Puzzel("wanneer is beeld x", "2"));
+                                    add(new Puzzel("waarom is dit een ding", "nee"));
+                                }},
+                                51.206264f, 4.413934f
                         ));
                     }},
                     60*60*5
@@ -56,7 +65,7 @@ public class DummyRepositoryRegios implements IDummyRepositoryRegios{
                                     add(new Puzzel("wat doet deze persoon", "lijntje coca cola"));
                                     add(new Puzzel("waarom", "steen"));
                                 }},
-                                51.217263f,4.421034f
+                                51.284836f, 4.500597f
                         ));
                         add(new Locatie(
                                 "park",
@@ -65,7 +74,16 @@ public class DummyRepositoryRegios implements IDummyRepositoryRegios{
                                     add(new Puzzel("wanneer is beeld t", "2"));
                                     add(new Puzzel("waarom is dit een iets", "misschien"));
                                 }},
-                                51.217263f,4.421034f
+                                51.288343f, 4.495046f
+                        ));
+                        add(new Locatie(
+                                "golfclub",
+                                new ArrayList<Puzzel>(){{
+                                    add(new Puzzel("wat is beeld y", "19"));
+                                    add(new Puzzel("wanneer is beeld t", "2"));
+                                    add(new Puzzel("waarom is dit een iets", "misschien"));
+                                }},
+                                51.296294f, 4.516631f
                         ));
                     }},
                     60*60*5
