f18d1b20c04703f050b5a62e061796ab8d03741e
Yorick Lardenoit <yorick.lardenoit@hotmail.com>
2018-11-10 17:31:06 +0100

    markers on map

--- MobApp/app/src/main/java/be/ap/eaict/geocapture/MapActivity.java
+++ MobApp/app/src/main/java/be/ap/eaict/geocapture/MapActivity.java
@@ -4,11 +4,41 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
+
+import be.ap.eaict.geocapture.Model.Locatie;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = "MapActivity";
 
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        DummyRepositoryRegios dummyRepositoryRegios = new DummyRepositoryRegios();
+        List<Locatie> locaties = dummyRepositoryRegios.getRegios().get(0).getLocaties();
+
+        for(Locatie locatie:locaties){
+            LatLng latLng = new LatLng(locatie.getLng(), locatie.getLat());
+            googleMap.addMarker(new MarkerOptions().position(latLng)
+                    .title(locatie.getLocatienaam()));
+        }
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(locaties.get(0).getLng(), locaties.get(0).getLat())));
+        googleMap.setMinZoomPreference(14);
     }
 }
