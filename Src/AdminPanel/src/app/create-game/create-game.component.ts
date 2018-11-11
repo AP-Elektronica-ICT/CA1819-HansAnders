04dab580ee43bdd2abc83d012904bb73441fb60a
Unknown <nemesruben@gmail.com>
2018-11-11 22:16:16 +0100

    merge

--- Src/AdminPanel/src/app/create-game/create-game.component.ts
+++ Src/AdminPanel/src/app/create-game/create-game.component.ts
@@ -11,22 +11,4 @@ export class CreateGameComponent implements OnInit {
 
   ngOnInit() {
   }
-  title: string = 'Click to add places';
-  lat: number = 51.228418;
-  lng: number = 4.4159007;
-
-  markers: position[] = new Array
-  clickMap($event){
-    console.log($event.coords.lat);
-    console.log($event.coords.lng);
-    this.markers.push({
-      lat:$event.coords.lat,
-      lng:$event.coords.lng
-    });
-  }
-  
-}
-export interface position{
-  lat: number,
-  lng: number
 }
