a552cdfe54d20c9a598ea5961a3c8b02fc682578
Ruben Nemes <nemesruben@gmail.com>
2018-12-13 09:20:23 +0100

    oeps er miste een file

--- Src/API/WebApplication5/WebApplication5/Hubs/GameSessionHub.cs
+++ Src/API/WebApplication5/WebApplication5/Hubs/GameSessionHub.cs
@@ -36,7 +36,7 @@ namespace WebApplication5.Hubs
 
         public void pushGameData(string lobbyId)
         {
-            Game game = _context.Games.Include(y => y.enabledLocaties).ThenInclude(t => t.puzzels).Include(t => t.teams).ThenInclude(p => p.Users).Include(t => t.teams).ThenInclude(o => o.CapturedLocaties).Include(l => l.regio).ThenInclude(m => m.locaties).ThenInclude(i => i.puzzels).Include(r => r.regio).SingleOrDefaultAsync(m => m.ID == id);
+            Game game = _context.Games.Include(y => y.enabledLocaties).ThenInclude(t => t.puzzels).Include(t => t.teams).ThenInclude(p => p.Users).Include(t => t.teams).ThenInclude(o => o.CapturedLocaties).Include(l => l.regio).ThenInclude(m => m.locaties).ThenInclude(i => i.puzzels).Include(r => r.regio).SingleOrDefault(m => m.ID == Convert.ToInt32(lobbyId));
 
             if (game != null)
             {
