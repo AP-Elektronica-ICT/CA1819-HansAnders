e36d8b9bc86a6d48922be6382fa4cf927b9d373d
Yorick Lardenoit <yorick.lardenoit@hotmail.com>
2019-01-21 16:17:08 +0100

    leaderboard fixes

--- MobApp/app/src/main/java/be/ap/eaict/geocapture/LeaderboardActivity.java
+++ MobApp/app/src/main/java/be/ap/eaict/geocapture/LeaderboardActivity.java
@@ -2,6 +2,12 @@ package be.ap.eaict.geocapture;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.List;
+
+import be.ap.eaict.geocapture.Model.CaptureLocatie;
+import be.ap.eaict.geocapture.Model.Team;
 
 
 public class LeaderboardActivity extends AppCompatActivity {
@@ -11,6 +17,30 @@ public class LeaderboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
+        final GameService _gameService = new GameService();
 
+        List<Team> teams = _gameService.game.teams;
+        int bestscore = 0;
+
+        for(Team team : teams) {
+            int score = 0;
+            for(CaptureLocatie loc :  team.getCapturedLocaties())
+                score += loc.score;
+            if (score > bestscore){
+                bestscore = score;
+            }
+        }
+
+        int score = 0;
+        for(CaptureLocatie loc :  teams.get(_gameService.team).capturedLocaties )
+            score += loc.score;
+
+        TextView besteTeam = (TextView) findViewById(R.id.txtGewonnen);
+        if (score == bestscore){
+            besteTeam.setText("YOU WON \n Your score is:" + score);
+        }
+        else {
+            besteTeam.setText("You LOST the game \n your score is:" + score);
+        }
     }
 }
